c75bfe8e033a807d7bd5e17c1667722d916c4b43
Horatiu Bran <bran.horatiu@gmail.com>
2023-03-07 20:40:45 +0200

    initial commit

---
+++ src/entry-client.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import { Router } from "./router";
+
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter>,
+    document.getElementById("app")
+);
